23dbda0fdea141e5e19171d7fab9e598c46d7d06
Liza Iutina <iutinaelizaveta@yandex.ru>
2023-03-25 00:40:24 +0300

    fix: page not found

--- src/components/PageNotFound.js
+++ src/components/PageNotFound.js
@@ -3,7 +3,7 @@ import Header from "./Header";
 function PageNotFound() {
   return (
     <>
-      <Header namelink="Назад" link="sign-up" />
+      <Header namelink="Назад" link="" />
       <main className="sign">
         <h1 className="sign__title">404 - Страница не найдена</h1>
       </main>
